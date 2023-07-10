500f21728b66b6f19e1bfdef308e289c3f0551a9
OctavitoMC <130078949+OctavitoMC@users.noreply.github.com>
2023-07-10 14:49:03 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
+alert("Si a contuniacion pones una edad menor a 9 se cierra esta pagina y lo que mandes en el login no se le enviara a nadie")
 let edad = prompt('Cual es tu Edad')
 if (edad < 9 ) {
     window.close()
